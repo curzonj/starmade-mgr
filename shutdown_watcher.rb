09b2f3758d64f596710bfdb442272db47e64b500
Ubuntu <ubuntu@ip-172-31-39-111.ec2.internal>
2015-08-09 01:23:55 +0000

    fixes

--- shutdown_watcher.rb
+++ shutdown_watcher.rb
@@ -3,7 +3,10 @@
 require 'net/http'
 require 'aws-sdk'
 
+puts Time.now
+
 count = `netstat -nt | grep 4242 | wc -l`.strip.to_i
+puts "current connections: #{count}"
 
 if count == 0
 	ip = Net::HTTP.get(URI.parse("http://169.254.169.254/latest/meta-data/public-ipv4"))
@@ -16,9 +19,11 @@ if count == 0
     }
   ]).reservations.first.instances.first
 
+puts "moving ip address"
 	ec2.associate_address(
 		instance_id: mgr_inst.instance_id,
 		public_ip: ip)
 
+puts "shutting down"
 	`sudo shutdown -h now`
 end
--- shutdown_watcher.sh
+++ shutdown_watcher.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+exec 1>> ~/shutdown.log
+exec 2>> ~/shutdown.log
+
+date
+
+set -e
+set -x
+
+export PATH=/usr/local/bin:$PATH
+
 cd $(dirname $0)
+
 bundle exec ./shutdown_watcher.rb
